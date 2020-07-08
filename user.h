036ae9a56d3d44bbd48f831f7838cbe8d9c7872f
Hossein <hs.heron221@gmail.com>
2020-07-08 23:21:17 +0430

    Update user.h for part 3

--- user.h
+++ user.h
@@ -24,6 +24,11 @@ char* sbrk(int);
 int sleep(int);
 int uptime(void);
 
+//my syscall
+int waitx(int *wtime , int *rtime );
+int nice(void);
+
+
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
